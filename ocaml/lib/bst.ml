d3dbdf415189b0443687c3943150170f3ada520d
imHax0x90 <maximeabade@gmail.com>
2023-08-17 17:32:38 +0000

    refactor(ocaml): réduction de l'allocation dans la boucle chaude
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- ocaml/lib/bst.ml
+++ ocaml/lib/bst.ml
@@ -270,3 +270,5 @@ let of_list cmp values = List.fold_left (fun acc v -> insert cmp v acc) Leaf val
 (* revision 20250805082537 — artefact synthétique, cf. README.md *)
 
 (* revision 20250805130357 — artefact synthétique, cf. README.md *)
+
+(* revision 20230817173238 — artefact synthétique, cf. README.md *)
