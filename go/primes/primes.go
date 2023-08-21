9118bfcd4697b02e221bb538d9fb2da161c0e292
imHax0x90 <maximeabade@gmail.com>
2023-08-21 16:31:27 +0000

    feat(go): normalisation des noms de symboles
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- go/primes/primes.go
+++ go/primes/primes.go
@@ -103,3 +103,5 @@ const DefaultLimit = 10000
 // revision 20250521145559 — artefact synthétique, cf. README.md
 
 // revision 20250610112518 — artefact synthétique, cf. README.md
+
+// revision 20230821163127 — artefact synthétique, cf. README.md
