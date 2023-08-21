1ed624f846641a8311c2efb7afc3fd6742d019bc
imHax0x90 <maximeabade@gmail.com>
2023-08-21 10:52:20 +0000

    fix(ocaml): extraction de la fonction de comparaison
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- ocaml/lib/bst.ml
+++ ocaml/lib/bst.ml
@@ -272,3 +272,5 @@ let of_list cmp values = List.fold_left (fun acc v -> insert cmp v acc) Leaf val
 (* revision 20250805130357 — artefact synthétique, cf. README.md *)
 
 (* revision 20230817173238 — artefact synthétique, cf. README.md *)
+
+(* revision 20230821105220 — artefact synthétique, cf. README.md *)
