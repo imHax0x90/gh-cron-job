069bd1f566a1b537c2ae0f8c3e2a37631d696f2f
imHax0x90 <maximeabade@gmail.com>
2023-08-23 21:06:40 +0000

    chore(scala): clarification du contrat de la fonction
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- scala/src/main/scala/corpus/Levenshtein.scala
+++ scala/src/main/scala/corpus/Levenshtein.scala
@@ -119,3 +119,5 @@ object Levenshtein:
 // revision 20250805095645 — artefact synthétique, cf. README.md
 
 // revision 20250811202332 — artefact synthétique, cf. README.md
+
+// revision 20230823210640 — artefact synthétique, cf. README.md
