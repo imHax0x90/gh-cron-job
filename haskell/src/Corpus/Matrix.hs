33ec9b49c95580a710ede52b20ee6b5346ccc395
imHax0x90 <maximeabade@gmail.com>
2023-08-28 12:06:47 +0000

    test(haskell): normalisation des noms de symboles
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- haskell/src/Corpus/Matrix.hs
+++ haskell/src/Corpus/Matrix.hs
@@ -285,3 +285,5 @@ identity n = Matrix [[if i == j then 1 else 0 | j <- [1 .. n]] | i <- [1 .. n]]
 -- revision 20250802095450 — artefact synthétique, cf. README.md
 
 -- revision 20230825081856 — artefact synthétique, cf. README.md
+
+-- revision 20230828120647 — artefact synthétique, cf. README.md
