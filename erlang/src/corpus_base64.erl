72e271b06b4f21ca404bfb8e786b40014a4a89a9
imHax0x90 <maximeabade@gmail.com>
2023-09-04 15:25:37 +0000

    refactor(erlang): couverture des cas dégénérés
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- erlang/src/corpus_base64.erl
+++ erlang/src/corpus_base64.erl
@@ -252,3 +252,5 @@ index(Char) ->
 % revision 20230830103656 — artefact synthétique, cf. README.md
 
 % revision 20230904100406 — artefact synthétique, cf. README.md
+
+% revision 20230904152537 — artefact synthétique, cf. README.md
