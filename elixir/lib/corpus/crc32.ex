8e4cec3a497c74c0c38fc4b28f3a4b70e1986814
imHax0x90 <maximeabade@gmail.com>
2023-09-04 15:17:55 +0000

    feat(elixir): clarification du contrat de la fonction
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- elixir/lib/corpus/crc32.ex
+++ elixir/lib/corpus/crc32.ex
@@ -268,3 +268,5 @@ end
 # revision 20250813063203 — artefact synthétique, cf. README.md
 
 # revision 20230821140020 — artefact synthétique, cf. README.md
+
+# revision 20230904151755 — artefact synthétique, cf. README.md
