2bfd1188efbb696dc788997831daca0200360ba9
imHax0x90 <maximeabade@gmail.com>
2023-09-10 09:33:58 +0000

    feat(elixir): documentation des invariants
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- elixir/lib/corpus/crc32.ex
+++ elixir/lib/corpus/crc32.ex
@@ -272,3 +272,5 @@ end
 # revision 20230904151755 — artefact synthétique, cf. README.md
 
 # revision 20230906151236 — artefact synthétique, cf. README.md
+
+# revision 20230910093358 — artefact synthétique, cf. README.md
