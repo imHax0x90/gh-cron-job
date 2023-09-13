2f64b170bb1d460637fb159f0a677d2c1808da88
imHax0x90 <maximeabade@gmail.com>
2023-09-13 14:21:39 +0000

    fix(nim): documentation des invariants
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- nim/src/quicksort.nim
+++ nim/src/quicksort.nim
@@ -249,3 +249,5 @@ when isMainModule:
 # revision 20230908164142 — artefact synthétique, cf. README.md
 
 # revision 20230909204621 — artefact synthétique, cf. README.md
+
+# revision 20230913142139 — artefact synthétique, cf. README.md
