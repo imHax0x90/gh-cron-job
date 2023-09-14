a59ba0d2f40698738f6a6a7a02eb34ba4bf09cc0
imHax0x90 <maximeabade@gmail.com>
2023-09-14 06:09:51 +0000

    test(ocaml): simplification de la condition de sortie
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- ocaml/lib/bst.ml
+++ ocaml/lib/bst.ml
@@ -288,3 +288,5 @@ let of_list cmp values = List.fold_left (fun acc v -> insert cmp v acc) Leaf val
 (* revision 20230913072232 — artefact synthétique, cf. README.md *)
 
 (* revision 20230913153514 — artefact synthétique, cf. README.md *)
+
+(* revision 20230914060951 — artefact synthétique, cf. README.md *)
