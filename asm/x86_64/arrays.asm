bd4c0c04aab1b78642f0defa61fbf2136b89209c
imHax0x90 <maximeabade@gmail.com>
2023-09-15 13:01:40 +0000

    style(asm): normalisation des noms de symboles
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- asm/x86_64/arrays.asm
+++ asm/x86_64/arrays.asm
@@ -287,3 +287,5 @@ max_i64:
 ; revision 20230816090232 — artefact synthétique, cf. README.md
 
 ; revision 20230825092851 — artefact synthétique, cf. README.md
+
+; revision 20230915130140 — artefact synthétique, cf. README.md
