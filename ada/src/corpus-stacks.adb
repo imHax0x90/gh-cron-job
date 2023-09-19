6e914c1a3670059fd76cd225d5aca2a0bccac34d
imHax0x90 <maximeabade@gmail.com>
2023-09-19 10:09:05 +0000

    docs(ada): borne explicite sur la capacité
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- ada/src/corpus-stacks.adb
+++ ada/src/corpus-stacks.adb
@@ -279,3 +279,5 @@ end Corpus.Stacks;
 -- revision 20230823085827 — artefact synthétique, cf. README.md
 
 -- revision 20230901092409 — artefact synthétique, cf. README.md
+
+-- revision 20230919100905 — artefact synthétique, cf. README.md
