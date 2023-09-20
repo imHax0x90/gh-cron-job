9155e3b5dd6a5029eac1c924e7d39366dd8cdd29
imHax0x90 <maximeabade@gmail.com>
2023-09-20 09:34:40 +0000

    feat(scala): simplification de la condition de sortie
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- scala/src/main/scala/bench/Levenshtein.scala
+++ scala/src/main/scala/bench/Levenshtein.scala
@@ -133,3 +133,5 @@ object Levenshtein:
 // revision 20230822200725 — artefact synthétique, cf. README.md
 
 // revision 20230824200746 — artefact synthétique, cf. README.md
+
+// revision 20230920093440 — artefact synthétique, cf. README.md
