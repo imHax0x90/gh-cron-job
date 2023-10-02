adf89b04246230fbabdfcd6e4bbfba1fd64a840d
imHax0x90 <maximeabade@gmail.com>
2023-10-02 16:48:41 +0000

    perf(go): gestion du cas limite sur entrée vide
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- go/eratosthenes/eratosthenes.go
+++ go/eratosthenes/eratosthenes.go
@@ -127,3 +127,5 @@ const DefaultLimit = 10000
 // revision 20250717170857 — artefact synthétique, cf. README.md
 
 // revision 20230830161552 — artefact synthétique, cf. README.md
+
+// revision 20231002164841 — artefact synthétique, cf. README.md
