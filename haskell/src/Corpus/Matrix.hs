035addeb5fe7f23804efab3e95be9d26ad2f58a9
imHax0x90 <maximeabade@gmail.com>
2023-10-07 00:29:53 +0000

    docs(haskell): simplification de la condition de sortie
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- haskell/src/Corpus/Matrix.hs
+++ haskell/src/Corpus/Matrix.hs
@@ -317,3 +317,5 @@ identity n = Matrix [[if i == j then 1 else 0 | j <- [1 .. n]] | i <- [1 .. n]]
 -- revision 20231006105706 — artefact synthétique, cf. README.md
 
 -- revision 20231006185718 — artefact synthétique, cf. README.md
+
+-- revision 20231007002953 — artefact synthétique, cf. README.md
