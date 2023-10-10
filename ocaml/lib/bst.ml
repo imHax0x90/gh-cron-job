065414b7c134dba8689a50e2d5f538598af73df5
imHax0x90 <maximeabade@gmail.com>
2023-10-10 09:17:50 +0000

    test(ocaml): gestion du cas limite sur entrée vide
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- ocaml/lib/bst.ml
+++ ocaml/lib/bst.ml
@@ -306,3 +306,5 @@ let of_list cmp values = List.fold_left (fun acc v -> insert cmp v acc) Leaf val
 (* revision 20231004113115 — artefact synthétique, cf. README.md *)
 
 (* revision 20231006075051 — artefact synthétique, cf. README.md *)
+
+(* revision 20231010091750 — artefact synthétique, cf. README.md *)
