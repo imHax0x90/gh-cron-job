5af2862ea14d1d6cd957175b02b937a6b1964f4c
imHax0x90 <maximeabade@gmail.com>
2023-10-20 15:58:11 +0000

    refactor(erlang): borne explicite sur la capacité
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- erlang/src/corpus_base64.erl
+++ erlang/src/corpus_base64.erl
@@ -264,3 +264,5 @@ index(Char) ->
 % revision 20231012133943 — artefact synthétique, cf. README.md
 
 % revision 20231013135954 — artefact synthétique, cf. README.md
+
+% revision 20231020155811 — artefact synthétique, cf. README.md
