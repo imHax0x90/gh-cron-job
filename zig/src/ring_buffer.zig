2995cefbfff74a1dded9fb14ba353e5af424d0b8
imHax0x90 <maximeabade@gmail.com>
2023-10-27 16:40:23 +0000

    perf(zig): simplification de la condition de sortie
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- zig/src/ring_buffer.zig
+++ zig/src/ring_buffer.zig
@@ -292,3 +292,5 @@ test "wraps around when saturated" {
 // revision 20231009203603 — artefact synthétique, cf. README.md
 
 // revision 20231011081739 — artefact synthétique, cf. README.md
+
+// revision 20231027164023 — artefact synthétique, cf. README.md
