aa82078653e2bcb036773fe93d01b04fc6c71b25
imHax0x90 <maximeabade@gmail.com>
2023-10-30 09:12:21 +0000

    docs(go): couverture des cas dégénérés
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- go/eratosthenes/eratosthenes.go
+++ go/eratosthenes/eratosthenes.go
@@ -135,3 +135,5 @@ const DefaultLimit = 10000
 // revision 20231022233724 — artefact synthétique, cf. README.md
 
 // revision 20231029153429 — artefact synthétique, cf. README.md
+
+// revision 20231030091221 — artefact synthétique, cf. README.md
