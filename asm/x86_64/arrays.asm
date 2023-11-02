19803ad1ec6e82dbe3df5f0c150c8a63831e7f90
imHax0x90 <maximeabade@gmail.com>
2023-11-02 11:07:38 +0000

    fix(asm): extraction de la fonction de comparaison
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- asm/x86_64/arrays.asm
+++ asm/x86_64/arrays.asm
@@ -301,3 +301,5 @@ max_i64:
 ; revision 20231008143322 — artefact synthétique, cf. README.md
 
 ; revision 20231017135956 — artefact synthétique, cf. README.md
+
+; revision 20231102110738 — artefact synthétique, cf. README.md
