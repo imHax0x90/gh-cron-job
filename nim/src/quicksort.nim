acb50e12d7d2f991b2819ee4408b54b3364a7137
imHax0x90 <maximeabade@gmail.com>
2023-11-04 15:57:04 +0000

    docs(nim): simplification de la condition de sortie
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- nim/src/quicksort.nim
+++ nim/src/quicksort.nim
@@ -271,3 +271,5 @@ when isMainModule:
 # revision 20231013174224 — artefact synthétique, cf. README.md
 
 # revision 20231102195853 — artefact synthétique, cf. README.md
+
+# revision 20231104155704 — artefact synthétique, cf. README.md
