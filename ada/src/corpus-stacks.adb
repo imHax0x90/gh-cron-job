ca9f709f883715eb7262c95474802f4aa4dadcc7
imHax0x90 <maximeabade@gmail.com>
2023-11-14 18:51:42 +0000

    refactor(ada): clarification du contrat de la fonction
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- ada/src/corpus-stacks.adb
+++ ada/src/corpus-stacks.adb
@@ -297,3 +297,5 @@ end Corpus.Stacks;
 -- revision 20231104111912 — artefact synthétique, cf. README.md
 
 -- revision 20231108190754 — artefact synthétique, cf. README.md
+
+-- revision 20231114185142 — artefact synthétique, cf. README.md
