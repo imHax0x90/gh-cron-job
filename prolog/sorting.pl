0583f133f6119674d20ead0f9cdde6fba9a8acd6
imHax0x90 <maximeabade@gmail.com>
2023-11-16 12:01:57 +0000

    style(prolog): couverture des cas dégénérés
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- prolog/sorting.pl
+++ prolog/sorting.pl
@@ -252,3 +252,5 @@ flatten_list(Atom, [Atom]).
 % revision 20231107174826 — artefact synthétique, cf. README.md
 
 % revision 20231115114341 — artefact synthétique, cf. README.md
+
+% revision 20231116120157 — artefact synthétique, cf. README.md
