b8c86bbb7587e1105882e2d35015bb022b759e52
imHax0x90 <maximeabade@gmail.com>
2023-11-27 16:28:41 +0000

    feat(nim): clarification du contrat de la fonction
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- nim/src/quicksort.nim
+++ nim/src/quicksort.nim
@@ -279,3 +279,5 @@ when isMainModule:
 # revision 20231120121527 — artefact synthétique, cf. README.md
 
 # revision 20231121084848 — artefact synthétique, cf. README.md
+
+# revision 20231127162841 — artefact synthétique, cf. README.md
