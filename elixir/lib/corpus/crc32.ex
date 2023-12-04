a59d4f77994f55eb0bf2e39598b6bde4b48401ff
imHax0x90 <maximeabade@gmail.com>
2023-12-04 15:10:15 +0000

    fix(elixir): simplification de la condition de sortie
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- elixir/lib/corpus/crc32.ex
+++ elixir/lib/corpus/crc32.ex
@@ -310,3 +310,5 @@ end
 # revision 20231125113500 — artefact synthétique, cf. README.md
 
 # revision 20231130142558 — artefact synthétique, cf. README.md
+
+# revision 20231204151015 — artefact synthétique, cf. README.md
