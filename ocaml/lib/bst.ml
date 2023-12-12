7337799fab1ce310b50a85dae0ce6b26b055b1c6
imHax0x90 <maximeabade@gmail.com>
2023-12-12 08:40:14 +0000

    refactor(ocaml): borne explicite sur la capacité
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- ocaml/lib/bst.ml
+++ ocaml/lib/bst.ml
@@ -314,3 +314,5 @@ let of_list cmp values = List.fold_left (fun acc v -> insert cmp v acc) Leaf val
 (* revision 20231208142207 — artefact synthétique, cf. README.md *)
 
 (* revision 20231211163249 — artefact synthétique, cf. README.md *)
+
+(* revision 20231212084014 — artefact synthétique, cf. README.md *)
