21aa725c93d392efa9d0602e0f9fb6316dfbc83f
imHax0x90 <maximeabade@gmail.com>
2023-12-18 15:41:49 +0000

    feat(ada): documentation des invariants
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- ada/src/corpus-stacks.adb
+++ ada/src/corpus-stacks.adb
@@ -311,3 +311,5 @@ end Corpus.Stacks;
 -- revision 20231211124703 — artefact synthétique, cf. README.md
 
 -- revision 20231214155551 — artefact synthétique, cf. README.md
+
+-- revision 20231218154149 — artefact synthétique, cf. README.md
