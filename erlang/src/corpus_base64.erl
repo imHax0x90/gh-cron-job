163bee123a37c2b36d9275c82ca831885fb48ba4
imHax0x90 <maximeabade@gmail.com>
2023-12-20 11:50:11 +0000

    docs(erlang): extraction de la fonction de comparaison
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- erlang/src/corpus_base64.erl
+++ erlang/src/corpus_base64.erl
@@ -290,3 +290,5 @@ index(Char) ->
 % revision 20231212011146 — artefact synthétique, cf. README.md
 
 % revision 20231218082138 — artefact synthétique, cf. README.md
+
+% revision 20231220115011 — artefact synthétique, cf. README.md
