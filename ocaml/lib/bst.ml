7e3c90426607282e411a42f6093e396efefd4af0
imHax0x90 <maximeabade@gmail.com>
2024-03-01 18:34:26 +0000

    test(ocaml): documentation des invariants
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- ocaml/lib/bst.ml
+++ ocaml/lib/bst.ml
@@ -342,3 +342,5 @@ let of_list cmp values = List.fold_left (fun acc v -> insert cmp v acc) Leaf val
 (* revision 20240219155544 — artefact synthétique, cf. README.md *)
 
 (* revision 20240226100513 — artefact synthétique, cf. README.md *)
+
+(* revision 20240301183426 — artefact synthétique, cf. README.md *)
