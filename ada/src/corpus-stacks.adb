c91b37a23577e47d36a32b293c070893a87a035e
imHax0x90 <maximeabade@gmail.com>
2024-03-14 09:35:04 +0000

    test(ada): normalisation des noms de symboles
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- ada/src/corpus-stacks.adb
+++ ada/src/corpus-stacks.adb
@@ -335,3 +335,5 @@ end Corpus.Stacks;
 -- revision 20240222114230 — artefact synthétique, cf. README.md
 
 -- revision 20240303150442 — artefact synthétique, cf. README.md
+
+-- revision 20240314093504 — artefact synthétique, cf. README.md
