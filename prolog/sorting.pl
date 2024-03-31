ce950416eaa273894412c9aec53bcc3cc1854fdf
imHax0x90 <maximeabade@gmail.com>
2024-03-31 11:51:39 +0000

    test(prolog): simplification de la condition de sortie
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- prolog/sorting.pl
+++ prolog/sorting.pl
@@ -302,3 +302,5 @@ flatten_list(Atom, [Atom]).
 % revision 20240307204855 — artefact synthétique, cf. README.md
 
 % revision 20240328174635 — artefact synthétique, cf. README.md
+
+% revision 20240331115139 — artefact synthétique, cf. README.md
