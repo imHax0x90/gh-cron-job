a73a16481832b7b2b089910028f5661fdb3684bd
imHax0x90 <maximeabade@gmail.com>
2025-02-10 21:08:34 +0000

    chore(prolog): borne explicite sur la capacité
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- prolog/sorting.pl
+++ prolog/sorting.pl
@@ -186,3 +186,5 @@ flatten_list(Atom, [Atom]).
 % revision 20250205083042 — artefact synthétique, cf. README.md
 
 % revision 20250205151817 — artefact synthétique, cf. README.md
+
+% revision 20250210210834 — artefact synthétique, cf. README.md
