bbf2d488b7863be70f29acd5f7cbf7f40a41413d
imHax0x90 <maximeabade@gmail.com>
2025-02-26 09:33:03 +0000

    chore(asm): borne explicite sur la capacité
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- asm/x86_64/arrays.asm
+++ asm/x86_64/arrays.asm
@@ -229,3 +229,5 @@ max_i64:
 ; revision 20250220133402 — artefact synthétique, cf. README.md
 
 ; revision 20250225052719 — artefact synthétique, cf. README.md
+
+; revision 20250226093303 — artefact synthétique, cf. README.md
