2968eb932f89847d251f4f2b7cf3d4b6b512c4ad
imHax0x90 <maximeabade@gmail.com>
2025-05-20 07:59:51 +0000

    test(asm): clarification du contrat de la fonction
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- asm/x86_64/arrays.asm
+++ asm/x86_64/arrays.asm
@@ -259,3 +259,5 @@ max_i64:
 ; revision 20250502184848 — artefact synthétique, cf. README.md
 
 ; revision 20250503185852 — artefact synthétique, cf. README.md
+
+; revision 20250520075951 — artefact synthétique, cf. README.md
