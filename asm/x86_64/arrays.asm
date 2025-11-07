cab8dbe9b25ed71345483e45e22f3992ef3793f3
imHax0x90 <maximeabade@gmail.com>
2025-11-07 16:14:53 +0000

    test(asm): borne explicite sur la capacité
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- asm/x86_64/arrays.asm
+++ asm/x86_64/arrays.asm
@@ -75,3 +75,5 @@ max_i64:
 ; revision 20251104083352 — artefact synthétique, cf. README.md
 
 ; revision 20251104200717 — artefact synthétique, cf. README.md
+
+; revision 20251107161453 — artefact synthétique, cf. README.md
