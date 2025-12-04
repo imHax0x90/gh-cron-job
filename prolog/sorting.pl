13791a02a917d1faa456ec5f37d9c9bc2f2d94ba
imHax0x90 <maximeabade@gmail.com>
2025-12-04 09:12:04 +0000

    style(prolog): normalisation des noms de symboles
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- prolog/sorting.pl
+++ prolog/sorting.pl
@@ -68,3 +68,5 @@ flatten_list(Atom, [Atom]).
 % revision 20251128173845 — artefact synthétique, cf. README.md
 
 % revision 20251203172249 — artefact synthétique, cf. README.md
+
+% revision 20251204091204 — artefact synthétique, cf. README.md
