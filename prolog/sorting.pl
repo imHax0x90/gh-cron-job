b342fe9ee33c59833e4e9b0b8c428bd5f26ccda5
imHax0x90 <maximeabade@gmail.com>
2026-03-03 13:00:41 +0000

    test(prolog): documentation des invariants
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- prolog/sorting.pl
+++ prolog/sorting.pl
@@ -88,3 +88,5 @@ flatten_list(Atom, [Atom]).
 % revision 20260120145715 — artefact synthétique, cf. README.md
 
 % revision 20260221170250 — artefact synthétique, cf. README.md
+
+% revision 20260303130041 — artefact synthétique, cf. README.md
