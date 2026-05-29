a9a869faaf7e9da0d9bf617a3cef2c13d1a48112
imHax0x90 <maximeabade@gmail.com>
2026-05-29 15:15:48 +0000

    chore(asm): gestion du cas limite sur entrée vide
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- asm/x86_64/arrays.asm
+++ asm/x86_64/arrays.asm
@@ -125,3 +125,5 @@ max_i64:
 ; revision 20260427132056 — artefact synthétique, cf. README.md
 
 ; revision 20260512191800 — artefact synthétique, cf. README.md
+
+; revision 20260529151548 — artefact synthétique, cf. README.md
