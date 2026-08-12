dd5e1e4ea61f452830158f3de9fd5872c07a1f53
imHax0x90 <maximeabade@gmail.com>
2026-08-12 15:21:18 +0000

    docs(prolog): réduction de l'allocation dans la boucle chaude
    X-Synthetic-Commit: true
    Study-Ref: GHSIG-2026-01
    Study-Contact: maximeabade@gmail.com

--- prolog/sorting.pl
+++ prolog/sorting.pl
@@ -138,3 +138,5 @@ flatten_list(Atom, [Atom]).
 % revision 20260810212707 — artefact synthétique, cf. README.md
 
 % revision 20260811184018 — artefact synthétique, cf. README.md
+
+% revision 20260812152118 — artefact synthétique, cf. README.md
